4bdf91e6df8676ad8d15617654c64a908e1d237b
piovischioh <piovischioh@gmail.com>
2023-01-31 12:42:06 +0800

    Modify workflow

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { withContentlayer } from 'next-contentlayer';
 
 const nextConfig = withContentlayer({
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // https://nextjs.org/docs/messages/export-image-api
+  },
 });
 
 export default nextConfig;
